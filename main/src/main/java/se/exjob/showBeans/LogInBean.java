f900525443d3ad76585e9368a9b01debfb6b335a
Mikael Lof <mickes511@hotmail.com>
2012-05-09 12:38:10 +0200

    test

--- main/src/main/java/se/exjob/showBeans/LogInBean.java
+++ main/src/main/java/se/exjob/showBeans/LogInBean.java
@@ -38,7 +38,7 @@ public class LogInBean {
             doneMessage = new FacesMessage("Password incorrect");
         } catch (ServerException e) {
             returnPage = "logIn.xhtml" ;
-            doneMessage = new FacesMessage(e.getStackTrace().toString());
+            doneMessage = new FacesMessage(e.getCause().getMessage());
         }
         FacesContext.getCurrentInstance().addMessage(null,doneMessage);
         return returnPage;
