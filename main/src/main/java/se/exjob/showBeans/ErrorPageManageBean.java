04436080ca678ee75050bdd117568561f4cc0246
Mikael Lof <mickes511@hotmail.com>
2012-05-16 11:32:00 +0200

    test Amazon

--- main/src/main/java/se/exjob/showBeans/ErrorPageManageBean.java
+++ main/src/main/java/se/exjob/showBeans/ErrorPageManageBean.java
@@ -3,18 +3,11 @@ package se.exjob.showBeans;
 import javax.faces.application.ConfigurableNavigationHandler;
 import javax.faces.context.FacesContext;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mikael.lof
- * Date: 2012-05-16
- * Time: 11:05
- * To change this template use File | Settings | File Templates.
- */
 public class ErrorPageManageBean {
     public void redirect(){
         FacesContext fc = FacesContext.getCurrentInstance();
         ConfigurableNavigationHandler nav = (ConfigurableNavigationHandler)
                 fc.getApplication().getNavigationHandler();
-        nav.performNavigation("errorPage");
+        nav.performNavigation("http://ec2-23-22-55-131.compute-1.amazonaws.com");
     }
 }
