8e5e5d7fa6d7e44866aba9c7335e80b62963706e
Mikael Lof <mickes511@hotmail.com>
2012-05-16 11:45:28 +0200

    test Amazon

--- main/src/main/java/se/exjob/showBeans/ErrorPageManageBean.java
+++ main/src/main/java/se/exjob/showBeans/ErrorPageManageBean.java
@@ -1,8 +1,12 @@
 package se.exjob.showBeans;
 
 import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+@ManagedBean
+@RequestScoped
 public class ErrorPageManageBean {
     public void redirect(){
         FacesContext fc = FacesContext.getCurrentInstance();
