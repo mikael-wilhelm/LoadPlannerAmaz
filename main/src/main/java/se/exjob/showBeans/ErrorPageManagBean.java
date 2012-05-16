b80bfd0bd60daab55b9fee4dfcec5436754a7ec1
Mikael Lof <mickes511@hotmail.com>
2012-05-16 11:07:52 +0200

    test Amazon

---
+++ main/src/main/java/se/exjob/showBeans/ErrorPageManagBean.java
@@ -0,0 +1,20 @@
+package se.exjob.showBeans;
+
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.context.FacesContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mikael.lof
+ * Date: 2012-05-16
+ * Time: 11:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class ErrorPageManagBean{
+    public void redirect(){
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ConfigurableNavigationHandler nav = (ConfigurableNavigationHandler)
+                fc.getApplication().getNavigationHandler();
+        nav.performNavigation("errorPage");
+    }
+}
