47eabdfa2811f10a7fd643d34a0dc5053eba1d2d
Mikael Lof <mickes511@hotmail.com>
2012-05-21 15:43:52 +0200

    test Amazon

---
+++ main/src/main/java/se/exjob/filter/LogoutFilter.java
@@ -0,0 +1,40 @@
+package se.exjob.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogoutFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest)request).getSession();
+
+        if (session.isNew()) {
+            session.setAttribute("start", System.currentTimeMillis());
+        }
+        else {
+            long start = (Long) session.getAttribute("start");
+
+            if (System.currentTimeMillis() - start > (30 * 60 * 1000)) {
+                session.invalidate();
+                ((HttpServletResponse) response).sendRedirect("ec2-184-73-16-97.compute-1.amazonaws.com");
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
